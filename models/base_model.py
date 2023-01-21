cec1348555116838f0f838b55fca0bae91d7d8a1
wrefinity <wrefordmessi@gmail.com>
2023-01-21 20:00:13 +0100

    user and based model updates for md5

--- models/base_model.py
+++ models/base_model.py
@@ -69,15 +69,15 @@ class BaseModel:
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         frame = inspect.currentframe().f_back
-        func_frame = frame.f_code.co_name
+        func_name = frame.f_code.co_name
         class_name = ''
         if 'self' in frame.f_locals:
             class_name = frame.f_locals["self"].__class__.__name__
-        is_fs_writing = func_frame == 'save' and class_name == 'FileStorage'
+        is_fs_writing = func_name == 'save' and class_name == 'FileStorage'
         if 'password' in new_dict and not is_fs_writing:
             del new_dict['password']
         return new_dict
 
     def delete(self):
-        """delete the present instance from the storage"""
+        """delete the current instance from the storage"""
         models.storage.delete(self)
